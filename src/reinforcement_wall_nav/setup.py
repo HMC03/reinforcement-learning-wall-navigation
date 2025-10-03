4dff4d9fe7aa38a85a7de463bb455071f17c8b62
Hayden <haydenmcameron@proton.me>
2025-10-02 23:13:25 -0400

    Update package.xml and setup.py

--- src/reinforcement_wall_nav/setup.py
+++ src/reinforcement_wall_nav/setup.py
@@ -7,9 +7,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/reinforcement_wall_nav.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
